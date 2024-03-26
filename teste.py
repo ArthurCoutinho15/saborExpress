0abcba248f362f56407016455915255cf87b6f38
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2024-03-26 10:14:50 -0300

    Finaliza projeto

--- teste.py
+++ teste.py
@@ -1,20 +1,23 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 
 restaurante_praca = Restaurante("Sal", "Brasileiro")
 bebida_suco = Bebida("Suco de melancia", 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato("Paozinho", 2.0, "O melhor pão da cidade")
-restaurante_praca.adicionar_prato_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
-
+prato_paozinho.aplicar_desconto()
+prato_sobremesa = Sobremesa("Panacotta", 35.0, "Morango", "Grande")
+prato_sobremesa.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(prato_sobremesa)
 
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
-
+    restaurante_praca.exibir_cardapio()
 
 if __name__ == '__main__':
     main()
